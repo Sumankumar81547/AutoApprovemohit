db9bc2e421fb2f1c66218f8ab937402dd506efba
Sumankumar81547 <168259000+Sumankumar81547@users.noreply.github.com>
2024-05-16 21:55:01 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,9 +4,9 @@ class Config:
     API_ID = int(getenv("API_ID", ""))
     API_HASH = getenv("API_HASH", "")
     BOT_TOKEN = getenv("BOT_TOKEN", "")
-    FSUB = getenv("FSUB", "Venom_Chatz")
+    FSUB = getenv("FSUB", "R_I_S_I_NG")
     CHID = int(getenv("CHID", ""))
-    SUDO = list(map(int, getenv("SUDO", "7048354045").split()))
+    SUDO = list(map(int, getenv("SUDO", "6615076069").split()))
     MONGO_URI = getenv("MONGO_URI", "")
     
 cfg = Config()
